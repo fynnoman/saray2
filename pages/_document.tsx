4d8c96575501c1220db472a846002fab5d72e161
Fynn Schulz <finolino9@gmail.com>
2026-04-28 02:11:57 +0200

    Add hidden link to taskeyapp.com

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,23 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        {/* Unsichtbarer, winziger Link */}
+        <a
+          href="https://www.taskeyapp.com"
+          style={{
+            position: "absolute",
+            left: "-9999px",
+            width: "1px",
+            height: "1px",
+            overflow: "hidden",
+            fontSize: "1px",
+            opacity: 0.01,
+          }}
+          tabIndex={-1}
+          aria-hidden="true"
+        >
+          taskey
+        </a>
       </body>
     </Html>
   );
